ce8b0ca0a95c55f72b2fdf12a32c0ca66d0af06e
yinziyang <yinziyang@users.noreply.github.com>
2026-01-27 14:52:38 +0800

    备份代码

--- middleware.ts
+++ middleware.ts
@@ -38,6 +38,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|sw.js|manifest.webmanifest|icons/.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
